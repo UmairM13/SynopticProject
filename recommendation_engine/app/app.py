3bb9e3c2dd21cc7cf65497cc5accc7a11c00c02b
UmairM13 <22493168@stu.mmu.ac.uk>
2025-03-06 11:01:33 +0000

    create user

--- recommendation_engine/app/app.py
+++ recommendation_engine/app/app.py
@@ -1,1 +1,16 @@
 from flask import Flask
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+app.register_blueprint(user_blueprint)
+app.register_blueprint(destination_blueprint)
+
+if __name__ == '__main__':
+    with app.app_context:
+        db.create_all()
+    app.run()
