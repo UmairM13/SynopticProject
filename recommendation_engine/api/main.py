8a294520d66206598c784db955415e6c954c4321
UmairM13 <22493168@stu.mmu.ac.uk>
2025-04-30 20:04:12 +0100

    updated CORS

--- recommendation_engine/api/main.py
+++ recommendation_engine/api/main.py
@@ -14,7 +14,9 @@ app = FastAPI()
 
 origins = [
     "http://localhost:5173",  
-    "http://127.0.0.1:5173"
+    "http://127.0.0.1:5173",
+    "https://travelm8.uk",
+    "https://www.travelm8.uk",
 ]
 
 app.add_middleware(
